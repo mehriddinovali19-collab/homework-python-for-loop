dcdac4a80b2428057deab0fb411adad128c0fa1b
ali mekhridinov <mehriddinovali19@gmail.com>
2026-03-17 13:24:16 +0500

    task12 done

---
+++ task12.py
@@ -0,0 +1,11 @@
+
+
+q_num =int(input("Qavat:"))
+
+total = 0
+for i in range(1,q_num+1):
+    balandlik = float(input(f"{i}-qavat balandligini kiriting (metrda): "))
+
+    total += balandlik
+
+    print(total)
